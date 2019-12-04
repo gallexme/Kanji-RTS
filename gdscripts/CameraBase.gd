45750911f8fd1e5d927576a2f65e3fc6ae2efa63
fink <lostchaos3@gmail.com>
2019-12-04 21:37:12 +0100

    fixed

--- gdscripts/CameraBase.gd
+++ gdscripts/CameraBase.gd
@@ -3,23 +3,36 @@ const MOVE_SPEED = 30
 const ray_length = 1000
 onready var cam = $Camera
 onready var  MOVE_MARGIN = get_viewport().get_size()*0.2
-
+var rotation_angle = 0.0
 func _process(delta):
 	var m_pos = get_viewport().get_mouse_position()
-	calc_move(m_pos,delta)
+	if Input.is_mouse_button_pressed(BUTTON_MIDDLE):
+		calc_rotation(m_pos,delta)
+	else:
+		calc_move(m_pos,delta)
 	if Input.is_action_just_pressed("main_command"):
 		move_all_units(m_pos)
+func calc_rotation(m_pos,delta):
+	var v_size = get_viewport().size
+	if m_pos.x < v_size.x/2:
+		rotation_angle +=0.01*delta
+	if m_pos.x > v_size.x/2:
+		rotation_angle -=0.01*delta
+	#rotation_angle = rotation_angle.rotated(Vector3(1,0,0),rotation_degrees.x)
+	#global_translate(rotation_angle*delta*MOVE_SPEED)
+	global_rotate(Vector3(0,1,0),rotation_angle)
+	pass
 func calc_move(m_pos,delta):
 	var v_size = get_viewport().size
 	var move_vec = Vector3()
 	if m_pos.x < MOVE_MARGIN.x:
-		move_vec.x -=1
-	if m_pos.y < MOVE_MARGIN.y:
-		move_vec.z -=1
-	if m_pos.x > v_size.x - MOVE_MARGIN.x:
-		move_vec.x +=1
-	if m_pos.y > v_size.y -  MOVE_MARGIN.y:
 		move_vec.z +=1
+	if m_pos.y < MOVE_MARGIN.y:
+		move_vec.x -=1
+	if m_pos.x > v_size.x - MOVE_MARGIN.x:
+		move_vec.z -=1
+	if m_pos.y > v_size.y -  MOVE_MARGIN.y:
+		move_vec.x +=1
 	move_vec = move_vec.rotated(Vector3(0,1,0),rotation_degrees.y)
 	global_translate(move_vec*delta*MOVE_SPEED)
 	
